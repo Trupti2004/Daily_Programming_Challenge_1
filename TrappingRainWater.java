50b5f028ef1079249f37b4e67f1221d68b83196b
Trupti2004 <kutetrupti123@gmail.com>
2024-09-15 18:10:02 +0530

    Add files via upload

---
+++ TrappingRainWater.java
@@ -0,0 +1,31 @@
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = height.length - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int waterTrapped = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                waterTrapped += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                waterTrapped += rightMax - height[right];
+            }
+        }
+
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap(height));  // Output: 6
+    }
+}
+
